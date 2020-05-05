ef0e079a5ee3de5b0b26c85abfe44d884992cc52
Margui <margui.berjon@gmail.com>
2020-05-05 15:36:32 +0200

    Funtion paint card in cart

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const cart = document.getElementById('carrito');
 
 const coursesList = document.getElementById('lista-cursos');
 
+const cartList = document.querySelector('#lista-carrito tbody');
+
 
 //Listeners
 
@@ -13,13 +15,36 @@ coursesList.addEventListener('click', addCourse);
 function addCourse(ev) {
   ev.preventDefault();
   if (ev.target.classList.contains('agregar-carrito')) {
-    const course = ev.target.parentElement.parentElement;
+    const card = ev.target.parentElement.parentElement;
 
-    readDataCourse(course);
+    readDataCourse(card);
   }
 
 }
 
-function readDataCourse(course) {
-  console.log(course);
+function readDataCourse(card) {
+  const info = {
+    image: card.querySelector('img').src,
+    name: card.querySelector('h4').innerText,
+    price: card.querySelector('.precio span').innerText,
+    id: card.querySelector('a').getAttribute('data-id'),
+  }
+  console.log(info);
+  paintCoursesInCart(info);
+
+}
+
+function paintCoursesInCart(card) {
+  const row = document.createElement('tr');
+
+  row.innerHTML = `<td>
+  <img scr="${card.image}"> 
+  </td>
+  <td>
+  ${card.name}
+  </td>
+  `;
+
+  cartList.appendChild(row)
+
 }
